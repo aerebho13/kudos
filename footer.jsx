ab55eef2a9b5b8447c6f086caba53ffee07c433b
aerebho13 <97203978+aerebho13@users.noreply.github.com>
2022-03-08 16:12:50 -0800

    Add files via upload

---
+++ footer.jsx
@@ -0,0 +1,14 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div className='Foot'>
+            <h5>
+                Copyright © 2022. KUDOS
+                <br />All rights reserved. Created by Aaron Erebholo.
+            </h5>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ quantityPicker.jsx
@@ -0,0 +1,24 @@
+import './quantityPicker.css';
+import {useState} from 'react';
+
+const QuantityPicker = () => {
+    let [quantity, setQuantity] = useState(1);
+
+    const handleDecrease = () => {
+        if(quantity>1)
+        setQuantity(quantity-1);
+    };
+    const handleIncrease = () => {
+        setQuantity(quantity+1);
+    };
+
+    return (
+        <div className='quantityPicker'>
+            <button onClick={handleDecrease}>-</button>
+            <label> {quantity} </label>
+            <button onClick={handleIncrease}>+</button>
+        </div>
+    );
+    };
+
+export default QuantityPicker;
